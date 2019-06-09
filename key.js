7af4f8069b04c8f560da0e766e6b6dbf69d01135
Nirav Patel <email@example.com>
2019-06-09 01:39:27 -0400

    port fix

---
+++ key.js
@@ -0,0 +1,1 @@
+export var key = "NTg3MTM5NDg2MjgzMjAyNTgw.XPyWmA.vjdc1E6ZYxFSFtU5umzHhIqdpk";
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
 
 client.on("message", message => {
   let msg = message.content.split(" ");
-  if (msg[0] === "!apexlvl") {
+  if (msg[0] === "!lvl") {
     axios
       .get(
         "http://api.mozambiquehe.re/bridge?version=2&platform=PC&player=" +
@@ -28,8 +28,36 @@ client.on("message", message => {
         message.channel.send("Player Not Found");
       });
   }
+
+  if (msg[0] === "!dmg") {
+    console.log(msg[1]);
+    axios
+      .get(
+        "http://api.mozambiquehe.re/bridge?version=2&platform=PC&player=" +
+          msg[1] +
+          "&auth=Kae5tcqrkg3A05ajxLA9"
+      )
+      .then(dat => {
+        // console.log("???");
+        let selected = dat.data.legends.selected;
+        selected = selected[Object.keys(selected)[0]];
+        if (selected.games_played && selected.damage) {
+          let damage = selected.damage / selected.games_played;
+          message.channel.send(
+            msg[1] + " damage per game is approx: " + damage
+          );
+        } else {
+          message.channel.send(
+            "On your selected character requip the banner: games played and total damage to see your average damage per game"
+          );
+        }
+      })
+      .catch(() => {
+        message.channel.send("Player Not Found");
+      });
+  }
 });
 
-client.login("NTg3MTM5NDg2MjgzMjAyNTgw.XPyOmQ._PT9JQl4yieIj25mvRXfOXHqIJg");
+client.login("NTg3MTM5NDg2MjgzMjAyNTgw.XPyX3Q._YSeFuFQavPV3iNq4h9gTGcoVA8");
 
-app.listen("4000", console.log("botrunning"));
+app.listen(process.env.PORT || 4000, console.log("botrunning"));
