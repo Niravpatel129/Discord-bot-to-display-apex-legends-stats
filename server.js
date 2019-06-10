a32fda788240eb8f61543ad894ea386555830260
Nirav Patel <email@example.com>
2019-06-10 15:13:48 -0400

    image fix

--- server.js
+++ server.js
@@ -2,12 +2,46 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const axios = require("axios");
+var Jimp = require("jimp");
+app.use("/exported-images", express.static("static"));
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+//end of jimp stuff
+
+//function to set the localimage to the character with the data on top
+function loadImageForSelectedCharacter(url, games, dmg, ad, name) {
+  var name = name.charAt(0).toUpperCase() + name.slice(1);
+  var games = "Games: " + games;
+  var dam = "Damage: " + dmg;
+  var ad = "ADR: " + ad;
+  return new Promise((resolve, reject) => {
+    Jimp.read(url)
+      .then(function(image) {
+        loadedImage = image;
+        return Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+      })
+      .then(function(font) {
+        loadedImage
+          .print(font, 100, 600, games, (err, image, { x, y }) => {
+            image.print(font, 100, 600 + 40, dam, (err, image, { x, y }) => {
+              image.print(font, 100, 600 + 80, ad, (err, image, { x, y }) => {
+                image.print(font, 100, 600 - 400, name);
+              });
+            });
+          })
+          .write("picture.png");
+      })
+      .then(() => {
+        console.log("1");
+        resolve("success");
+      });
+  });
+}
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("bot running");
 });
 
 client.on("ready", () => {
@@ -42,7 +76,6 @@ client.on("message", message => {
           "&auth=Kae5tcqrkg3A05ajxLA9"
       )
       .then(dat => {
-        // console.log("???");
         let selected = dat.data.legends.selected;
         character = Object.keys(selected);
         console.log(character);
@@ -50,9 +83,27 @@ client.on("message", message => {
         if (selected.games_played && selected.damage) {
           let damage = selected.damage / selected.games_played;
           damage = Math.floor(damage);
-          message.channel.send(
-            msg[1] + " damage per game is approx " + damage + " on " + character
-          );
+          let img = selected.ImgAssets.icon;
+          let playername = msg[1];
+          const getResult = async () => {
+            return await loadImageForSelectedCharacter(
+              img,
+              selected.games_played,
+              selected.damage,
+              damage,
+              playername
+            );
+          };
+          getResult().then(() => {
+            console.log("2");
+            message.channel.send({
+              files: ["picture.png"]
+            });
+          });
+
+          // message.channel.send(
+          //   msg[1] + " damage per game is approx " + damage + " on " + character
+          // );
         } else {
           message.channel.send(
             "On your selected character equip the banner: games played and total damage to see your average damage per game"
@@ -67,4 +118,4 @@ client.on("message", message => {
 
 client.login("NTg3MTM5NDg2MjgzMjAyNTgw.XPyX3Q._YSeFuFQavPV3iNq4h9gTGcoVA8");
 
-app.listen(process.env.PORT || 4000, console.log("botrunning"));
+app.listen(process.env.PORT || 4001, console.log("botrunning"));
